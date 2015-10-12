3bd9405d71f8aa7d935f2cc802ba4cb9495b87aa
dolerovax <dolerovax@yandex.com>
2015-10-12 18:07:18 +1000

    token receiving is changed

--- mailcloudapi.py
+++ mailcloudapi.py
@@ -4,6 +4,7 @@ import requests
 #from urllib import parse
 import urllib
 import mcsettings as URL
+import re
 
 class Cloud():
 	"""main class with mail.ru cloud APIs"""
@@ -16,7 +17,7 @@ class Cloud():
 		self.token = ""
 		self.cookies = {}
 		self.loader = ""
-		
+
 	def __auth__(self):
 		values = {"Login":self.email, "Password":self.__password__}
 		self.response = requests.get(URL.AUTH, params = values)
@@ -28,10 +29,11 @@ class Cloud():
 
 	def __get_token__(self):
 		"""update token"""
-		values = {"email":self.email}
-		self.response = requests.get(URL.TOKEN, params = values, cookies = self.cookies)
-		self.token =  json.loads(self.response.text)["body"]["token"].strip()
-		if self.token:
+		self.response = requests.get(URL.CLOUD, cookies = self.cookies)
+		text = self.response.text
+		m = re.search('"token": "(\w+)"', text)
+		if m:
+			self.token = m.group(0)
 			return True
 		else:
 			return False
@@ -65,18 +67,16 @@ class Cloud():
 			values = {"Content_Type":"multipart/form-data", "Content" : {"file":filename}}
 			self.response = requests.post(self.loader, params = values, files = fd, cookies = self.cookies)
 
-#		print("load res" + self.response.text)
 		ls = self.response.text.split(";")
-#		print("load res" + self.response.text)
 
 		if self.response.status_code == requests.codes.ok and ls:
 			return {"name":filename,
-					"hash":ls[0].strip(), 
+					"hash":ls[0].strip(),
 					"size":ls[2].strip()
 					}
 		else:
 			return None
-		
+
 
 	def __link_file__(self,file_params,cloud_path):
 		"""Finaly add link of loaded file into cloud"""
@@ -91,7 +91,6 @@ class Cloud():
 				"x-email": self.email,
 				"token": self.token,
 				})
-#		print body
 		self.response = requests.post(URL.ADDFILE, data = body, cookies = self.cookies)
 		if self.response.status_code == requests.codes.ok:
 			return True
@@ -101,9 +100,9 @@ class Cloud():
 	def add_folder(self,full_folder_name = ""):
 		"""Generate full path to file every time, even if exist """
 		ls  = full_folder_name.split('/')
-		cls = [item for item in ls if item]	
+		cls = [item for item in ls if item]
 		parpath = ""
-		for foldername, parent in self.__gen_parents__(cls):	
+		for foldername, parent in self.__gen_parents__(cls):
 			parpath+="/"+parent
 			body = urllib.urlencode({
 					"add": json.dumps([{
@@ -141,7 +140,6 @@ class Cloud():
 		 """
 		self.__get_loader__()
 		params = self.__load_file__(local_path)
-#		print "", params
 		self.__link_file__(params,cloud_path)
 
 	def share(self,filename_with_path=""):
@@ -158,20 +156,20 @@ class Cloud():
 			return self.response.json()["body"][0]["url"]["get"]
 		else:
 			return None
-	
+
 	def load_folder(self,local_path,cloud_path):
 		"""Load folder into cloud recursively"""
 		abs_path = os.path.abspath(local_path)
 		local_path_length = len(abs_path) + 1
-		
+
 		self.add_folder(cloud_path)
-		
+
 		for root, dirs, files in os.walk(abs_path):
-		    local_current_dir = root[local_path_length:]
-		    cloud_current_dir = cloud_path + '/' + local_current_dir
-		    self.add_folder(cloud_current_dir)
-		    for file in files:
-			    self.add_file(root + '/' + file, cloud_current_dir)
+			local_current_dir = root[local_path_length:]
+			cloud_current_dir = cloud_path + '/' + local_current_dir
+			self.add_folder(cloud_current_dir)
+			for file in files:
+				self.add_file(root + '/' + file, cloud_current_dir)
 
 	def unshare(self, filename_with_path=""):
 		"""unshare the file """
